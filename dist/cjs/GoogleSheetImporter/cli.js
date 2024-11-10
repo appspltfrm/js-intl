ea65c9d029038f7567f7bede303d7bb4452e3f23
Mark Chris Levy <levy@codeandmore.pl>
2024-11-10 15:39:53 +0100

    commit

--- dist/cjs/GoogleSheetImporter/cli.js
+++ dist/cjs/GoogleSheetImporter/cli.js
@@ -3,16 +3,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const yargs_1 = tslib_1.__importDefault(require("yargs"));
+const helpers_1 = require("yargs/helpers");
 const GoogleSheetImporter_1 = require("./GoogleSheetImporter");
-const argv = (0, yargs_1.default)()
-    .option("outputPath", { description: "Output directory path", type: "string" })
-    .option("outputType", { description: "Output type", choices: ["json", "ts"], type: "string" })
-    .option("defaultLocale", { description: "Default locale to use, when no translation for some other locale", "type": "string" })
-    .option("document", { description: "Google document identifier", type: "string" })
-    .option("worksheet", { description: "Document's worksheet index (1 based) or worksheet name", type: "string" })
-    .option("filterTags", { description: "Comma separated tags, that must be present for keys from worksheet. Key will be taken from worksheet if at least one tag is matched." })
-    .demandOption(["outputPath", "outputType", "document"]).argv;
 (async () => {
+    const argv = await (0, yargs_1.default)((0, helpers_1.hideBin)(process.argv))
+        .option("outputPath", { description: "Output directory path", type: "string" })
+        .option("outputType", { description: "Output type", choices: ["json", "ts"], type: "string" })
+        .option("defaultLocale", { description: "Default locale to use, when no translation for some other locale", "type": "string" })
+        .option("document", { description: "Google document identifier", type: "string" })
+        .option("worksheet", { description: "Document's worksheet index (1 based) or worksheet name", type: "string" })
+        .option("filterTags", { description: "Comma separated tags, that must be present for keys from worksheet. Key will be taken from worksheet if at least one tag is matched." })
+        .demandOption(["outputPath", "outputType", "document"]).argv;
     const importer = new GoogleSheetImporter_1.GoogleSheetImporter();
     importer.outputPath = argv["outputPath"];
     importer.outputType = argv["outputType"];
@@ -29,5 +30,6 @@ const argv = (0, yargs_1.default)()
     catch (error) {
         console.error(error);
     }
+    process.exit();
 })();
 //# sourceMappingURL=cli.js.map
--- src/GoogleSheetImporter/cli.ts
+++ src/GoogleSheetImporter/cli.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-
+import {hideBin} from "yargs/helpers";
 import {GoogleSheetImporter} from "./GoogleSheetImporter";
 
-const argv = yargs()
-    .option("outputPath", {description: "Output directory path", type: "string"})
-    .option("outputType", {description: "Output type", choices: ["json", "ts"], type: "string"})
-    .option("defaultLocale", {description: "Default locale to use, when no translation for some other locale", "type": "string"})
-    .option("document", {description: "Google document identifier", type: "string"})
-    .option("worksheet", {description: "Document's worksheet index (1 based) or worksheet name", type: "string"})
-    .option("filterTags", {description: "Comma separated tags, that must be present for keys from worksheet. Key will be taken from worksheet if at least one tag is matched."})
-    .demandOption(["outputPath", "outputType", "document"]).argv;
-
 (async () => {
+
+    const argv = await yargs(hideBin(process.argv))
+        .option("outputPath", {description: "Output directory path", type: "string"})
+        .option("outputType", {description: "Output type", choices: ["json", "ts"], type: "string"})
+        .option("defaultLocale", {description: "Default locale to use, when no translation for some other locale", "type": "string"})
+        .option("document", {description: "Google document identifier", type: "string"})
+        .option("worksheet", {description: "Document's worksheet index (1 based) or worksheet name", type: "string"})
+        .option("filterTags", {description: "Comma separated tags, that must be present for keys from worksheet. Key will be taken from worksheet if at least one tag is matched."})
+        .demandOption(["outputPath", "outputType", "document"]).argv;
 
     const importer = new GoogleSheetImporter();
     importer.outputPath = argv["outputPath"];
@@ -27,9 +27,12 @@ const argv = yargs()
     for (let i = 0; i < documents.length; i++) {
         importer.addDocument(documents[i], sheets.length > i ? sheets[i] : undefined, tags.length > i ? (tags[i] ? tags[i].split(",") : undefined) : undefined);
     }
+
     try {
         await importer.generate();
     } catch (error) {
         console.error(error);
     }
+
+    process.exit();
 })();
