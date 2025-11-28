afb8b0d052acc26998e374c01c5daac46fb5c682
Mark Chris Levy <levy@codeandmore.pl>
2025-11-28 09:38:54 +0100

    commit

--- dist/currencyLocalizedName.js
+++ dist/currencyLocalizedName.js
@@ -1,5 +1,5 @@
 import { formatCurrency } from "./formatCurrency.js";
 export function currencyLocalizedName(currency) {
-    return formatCurrency([currency, 2], { currencyDisplay: "name", maximumSignificantDigits: 1, minimumSignificantDigits: 1 }).replace(/\w*\d\w*/, "");
+    return formatCurrency([currency, 2], { currencyDisplay: "name", maximumSignificantDigits: 1, minimumSignificantDigits: 1 }).replace(/\W*\d+\W*/, "");
 }
 //# sourceMappingURL=currencyLocalizedName.js.map
--- src/currencyLocalizedName.ts
+++ src/currencyLocalizedName.ts
@@ -2,5 +2,5 @@ import {Currency} from "./Currency.js";
 import {formatCurrency} from "./formatCurrency.js";
 
 export function currencyLocalizedName(currency: string | Currency) {
-    return formatCurrency([currency, 2], {currencyDisplay: "name", maximumSignificantDigits: 1, minimumSignificantDigits: 1}).replace(/\w*\d\w*/, "");
+    return formatCurrency([currency, 2], {currencyDisplay: "name", maximumSignificantDigits: 1, minimumSignificantDigits: 1}).replace(/\W*\d+\W*/, "");
 }
